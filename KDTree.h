d5773d3433257aa3705db526a297c2b3a3df13b8
richard978 <33021381+richard978@users.noreply.github.com>
2018-01-26 16:49:37 +0800

    Update KDTree.h

--- KDTree.h
+++ KDTree.h
@@ -10,7 +10,6 @@
 #include <map>
 #include <fstream>
 using namespace std;
-/*这里可以添加需要的代码*/
 
 class KDTree {
 public:
@@ -55,28 +54,27 @@ public:
 
 	int countNode(Node *root);
 	
-	bool buildTree(  //建树（利用方差选取维度法） 
+	bool buildTree(
 		int n,
 		int d,
 		float** data);
 
 	void _buildTree(int d, Node* &nodes, vector<vector<float> > list);
 
-	int search(    //查询（要有回溯过程） 
+	int search(
 		int d,
 		float* query);
 
 	int _search(vector<float> query, Node* tree, int d);
 
-	bool storeTree(    //写到外存 (二进制文件） 
+	bool storeTree(
 		const char* index_path);
 		
 	void _storeTree(const Node *ptr, int pos);
 
-	bool restoreTree(   //读进内存 
+	bool restoreTree(
 		const char* index_path);
 
-	/*可以添加需要的代码*/
 	int getDimen(vector<vector<float> > v, int n);
 
 	void split(Node* &nodes, int dimen, vector<vector<float> > list, vector<vector<float> > &leftlist, vector<vector<float> > &rightlist, int d);
